d460f112c92bf342c43f4618d403a8db7ca2885e
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@bidroom.com>
2022-02-03 19:10:58 +0100

    further reader impl

--- reader.go
+++ reader.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"io"
+
+	"golang.org/x/crypto/md4"
 )
 
 type ReaderSeeker interface {
@@ -16,6 +18,7 @@ type bufferedReader struct {
 	buffer       []byte
 	length       uint32
 	offset       int64
+	eof          bool
 }
 
 func NewBufferedReader(windowLength uint32, readerSeeker ReaderSeeker) bufferedReader {
@@ -31,6 +34,9 @@ func (br *bufferedReader) ReadWindow() error {
 	if err != nil && errors.Is(err, io.EOF) {
 		return err
 	}
+	if errors.Is(err, io.EOF) {
+		br.eof = true
+	}
 	br.length = uint32(readBytes)
 	br.offset, err = br.Seek(0, 1)
 	return err
@@ -42,4 +48,26 @@ func (br *bufferedReader) Offset() int64 {
 
 func (br *bufferedReader) Len() uint32 {
 	return br.length
+}
+
+func (br *bufferedReader) SumBufferBytes() uint32 {
+	var sum uint32
+	for i := uint32(0); i < br.length; i++ {
+		sum += uint32(br.buffer[i])
+	}
+	return sum
+}
+
+func (br *bufferedReader) isEOF() bool {
+	return br.eof
+}
+
+func (br *bufferedReader) MD4() []byte {
+	h := md4.New()
+	h.Write(br.buffer[:len(br.buffer)-int(br.length)])
+	return h.Sum(nil)
+}
+
+func (br *bufferedReader) Get(index int) byte {
+	return br.buffer[index]
 }
